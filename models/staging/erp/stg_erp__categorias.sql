4a24a719a79ce9594fec7a712b228c5d2cfdabec
André Luiz Braga Mallmann <alandre6721@gmail.com>
2024-12-26 20:01:05 +0000

    criação da dimensão produto

---
+++ models/staging/erp/stg_erp__categorias.sql
@@ -0,0 +1,15 @@
+with 
+    src_categorias as (
+        select *
+        from {{ source('erp', 'category') }}
+    )
+
+    , renomeado as (
+        select
+        cast( ID as int) as pk_categoria
+        , cast(CATEGORYNAME as string) as nome_categoria 
+        , cast(DESCRIPTION as string) as descricao_categoria
+        from src_categorias
+    )
+    
+    select * from renomeado
